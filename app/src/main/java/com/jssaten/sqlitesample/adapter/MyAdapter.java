cfdd1ff7bad9ddf88647b65a330d0db08bb8eed8
ashwani <mutationapps@gmail.com>
2017-06-09 10:41:30 +0530

    First Commi

---
+++ app/src/main/java/com/jssaten/sqlitesample/adapter/MyAdapter.java
@@ -0,0 +1,20 @@
+package com.jssaten.sqlitesample.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ashwani on 9/6/17.
+ */
+
+public class MyAdapter {
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        public MyViewHolder(View itemView){
+            super(itemView);
+
+        }
+
+    }
+}
